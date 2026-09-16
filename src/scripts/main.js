795849314e8105e7694d926c02b3ead75eb830b4
DouglasLopesDRL <douglasribeirodrl@gmail.com>
2026-09-15 23:37:36 -0300

    Criando a contagem regressiva e ajustando o package.json para criar a build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dateToEvent = new Date("Dec 25, 2026 20:00:00");
+const timeStampToEvent = dateToEvent.getTime();
+
+const countTime = setInterval(function(){
+    const now = new Date();
+    const timeStampNow = now.getTime();
+
+    const distanceDaysToEvent = timeStampToEvent - timeStampNow;
+
+    const daysInMs = 1000 * 60 * 60 * 24;
+    const hoursInMs = 1000 * 60 * 60;
+    const minutesInMs = 1000 * 60;
+
+    const daystoEvent = Math.floor(distanceDaysToEvent / daysInMs);
+    const hourstoEvent = Math.floor((distanceDaysToEvent % daysInMs) / hoursInMs);
+    const minutesToEvent = Math.floor((distanceDaysToEvent % hoursInMs) / minutesInMs);
+    const secondsToEvent = Math.floor((distanceDaysToEvent % minutesInMs) / 1000);
+
+    document.getElementById("countdown").innerHTML = `${daystoEvent}d ${hourstoEvent}h ${minutesToEvent}m ${secondsToEvent}s`;
+
+    if(distanceDaysToEvent < 0){
+        clearInterval(countTime);
+        document.getElementById("countdown").innerHTML = "O melhor dia já passou!";
+    }
+    
+}, 1000);
